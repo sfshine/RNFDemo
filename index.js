8ad064c636f465d99f46598e7aff7aaabe274d03
gaoshuai09 <gaoshuai09@meituan.com>
2021-02-14 21:22:18 +0800

    优化Js代码

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './example/App';
-import {name as appName} from './app.json';
+import App from './src/App';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('RNFDemo', () => App);
